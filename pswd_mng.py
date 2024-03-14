0a4b1610430b65b1a51a993ad4f1917735757a75
shebelaoleh <163007769+shebelaoleh@users.noreply.github.com>
2024-03-14 16:27:49 +0100

    Add files via upload

---
+++ pswd_mng.py
@@ -0,0 +1,44 @@
+#pip install cryptography
+
+from cryptography.fernet import Fernet
+
+
+
+hlavni_heslo=input("heslo: ")
+
+def klice():
+    klic=Fernet.generate_klic()
+    with open("klic.txt","wb") as klic_soubor:
+        klic_soubor.write(klic) 
+
+
+klice()
+
+
+def view():  
+    with open("hesla.txt","r") as f:
+        for line in f.readlines():
+            data=line.rstrip()
+            jmeno,heslo=data.split("/")
+            print("uzivatelske jemno:",jmeno,"•","heslo:",heslo)
+
+def add():
+    jmeno=input("uzivaelse jmeno: ")
+    heslo=input("heslo: ")
+
+    with open("hesla.txt", "a") as f:
+        f.write(jmeno+"/"+heslo+"\n")
+
+
+while True:
+    
+    mod=input("pridat heslo, nebo si prohlidnout hesla(view/add) nebo zmackni e pro exit: ").lower()
+    if mod=="e":
+        break
+
+    if mod=="view":
+        view()
+    elif mod=="add":
+        add()
+    else:
+        print("spatna operace")
